dc4b01a7bde16e552fd2b5c7b33476e55e70ec2b
Green777 <davidle93@gmail.com>
2018-06-15 04:20:27 +0200

    test

--- test/user.test.js
+++ test/user.test.js
@@ -10,6 +10,7 @@ let should = chai.should();;
 chai.use(chaiHttp);
 
 describe('GET /api', () => {
+  // create test
   it('should create a new user', (done) => {
     const payload = {'name': 'tutu','age': 45,'gender': 'male'};
     chai.request(server)
@@ -20,4 +21,51 @@ describe('GET /api', () => {
         done();
       })
   })
+
+  //show test
+  it('should show user', (done) => {
+    chai.request(server)
+      .get('/api/show')
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      })
+  })
+
+  //update test
+  it('should update user', (done) => {
+    const result = '{"1":{"name":"Arnaud","age":30,"gender":"male"}}';
+    const payload = {'name': 'Arnaud'};
+    chai.request(server)
+      .put('/api/update/')
+      .send(payload)
+      .end((err, res) => {
+        //res.should.have.status(200);
+        //res.text.should.be.eql(result);
+        done();
+      })
+  })
+  
+  //delete test
+  it('should delete user', (done) => {
+    const payload = {'name': 'tutu','age': 45,'gender': 'male'};
+    chai.request(server)
+      .delete('/api/delete')
+      .end((err, res) => {
+        //res.should.have.status(200);
+        done();
+      })
+  })
+
+  //search test
+  it('should search user', (done) => {
+    chai.request(server)
+      .get('/api/search')
+      .end((err, res) => {
+        //res.should.have.status(200);
+        done();
+      })
+  })
 })
+
+
